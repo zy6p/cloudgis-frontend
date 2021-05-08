47676342717c9a53c0ca9ed78c23c9a24d83bd58
knowmore <goooog@88.com>
2021-05-08 22:56:46 +0800

    Fix: Action ssh args

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 yarn build
-ssh $SERVER_USER@$SERVER_HOST -p $SREVER_PORT
-if [ -d "$SERVER_DEPLOY_DIR/cloudgis" ]; then
-  rm -r $SERVER_DEPLOY_DIR/cloudgis
-fi
-exit
-scp -r -P $SREVER_PORT ./dist $SERVER_USER@$SERVER_HOST:$SERVER_DEPLOY_DIR/cloudgis
+source ${PWD}/.env.local
+ssh -p $SERVER_SSH_PORT $SERVER_USER@$SERVER_HOST "trash-put $SERVER_DEPLOY_DIR/cloudgis"
+scp -r -P $SERVER_SSH_PORT ./dist $SERVER_USER@$SERVER_HOST:$SERVER_DEPLOY_DIR/cloudgis
+
+cd dist || exit
+git init
+git add -A
+git commit -a -m "deploy"
+git push -f git@github.com:zy6p/cloudgis-frontend.git master:gh-pages
