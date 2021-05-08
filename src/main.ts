3f998b01a78d7778afa77feea402e6d4aed509fc
knowmore <goooog@88.com>
2021-05-08 09:25:43 +0800

    add matomo analyse

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Axios from "axios";
+import VueAxios from "vue-axios";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import "@/m/matomo";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(VueAxios, Axios).mount("#app");
